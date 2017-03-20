openwrt/mdnsd

commit f89986b67dd5256c6570e563c81aded4d39bc002

Fix refreshing cached A(AAA) records that expire
Old code was trying to refresh record after it has already expired. Now
it tries after 50% of TTL has passed - we do the same for other records.

Fixes: 80dd24602480 ("Refresh DNS records A and AAAA directly")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
Acked-by: John Crispin <john@phrozen.org>

cache.c
@@ -76,7 +76,7 @@ cache_gc_timer(struct uloop_timeout *timeout)
 	struct cache_service *s, *t;
 
 	avl_for_each_element_safe(&records, r, avl, p) {
-		if (!cache_is_expired(r->time, r->ttl, 100))
+		if (!cache_is_expired(r->time, r->ttl, r->refresh))
 			continue;
 		/* Records other and A(AAA) are handled as services */
 		if (r->type != TYPE_A && r->type != TYPE_AAAA) {
@@ -336,9 +336,11 @@ cache_answer(struct interface *iface, uint8_t *base, int blen, char *name, struc
 		if (!a->ttl) {
 			DBG(1, "D -> %s %s ttl:%d\n", dns_type_string(r->type), r->record, r->ttl);
 			r->time = now + 1 - r->ttl;
+			r->refresh = 100;
 		} else {
 			r->ttl = a->ttl;
 			r->time = now;
+			r->refresh = 50;
 			DBG(1, "A -> %s %s ttl:%d\n", dns_type_string(r->type), r->record, r->ttl);
 		}
 		return;
@@ -359,6 +361,7 @@ cache_answer(struct interface *iface, uint8_t *base, int blen, char *name, struc
 	r->rdlength = dlen;
 	r->time = now;
 	r->iface = iface;
+	r->refresh = 50;
 
 	if (tlen)
 		r->txt = memcpy(txt_ptr, rdata_buffer, tlen);

commit 1478293fd95f29f16887c5e6554035cd38d4b2d7

Fix code freeing cached non-A(AAA) records too early
Fixes: f89986b67dd5 ("Fix refreshing cached A(AAA) records that expire")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

cache.c
@@ -80,7 +80,8 @@ cache_gc_timer(struct uloop_timeout *timeout)
 			continue;
 		/* Records other than A(AAA) are handled as services */
 		if (r->type != TYPE_A && r->type != TYPE_AAAA) {
-			cache_record_free(r);
+			if (cache_is_expired(r->time, r->ttl, 100))
+				cache_record_free(r);
 			continue;
 		}
 		if (r->refresh >= 100) {