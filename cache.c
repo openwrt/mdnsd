openwrt/mdnsd

commit 80dd246024800b3f33bacc6cf01184da6f57dfa4

Refresh DNS records A and AAAA directly
So far records A and AAAA were being connected and stored as a single
*service*. It's possible to handle these records directly without this
service trick and it also provides better control over them (some corner
cases like different TTL values).

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

cache.c
@@ -75,9 +75,21 @@ cache_gc_timer(struct uloop_timeout *timeout)
 	struct cache_record *r, *p;
 	struct cache_service *s, *t;
 
-	avl_for_each_element_safe(&records, r, avl, p)
-		if (cache_is_expired(r->time, r->ttl, 100))
+	avl_for_each_element_safe(&records, r, avl, p) {
+		if (!cache_is_expired(r->time, r->ttl, 100))
+			continue;
+		/* Records other and A(AAA) are handled as services */
+		if (r->type != TYPE_A && r->type != TYPE_AAAA) {
 			cache_record_free(r);
+			continue;
+		}
+		if (r->refresh >= 100) {
+			cache_record_free(r);
+			continue;
+		}
+		r->refresh += 50;
+		dns_send_question(r->iface, r->record, r->type, 0);
+	}
 
 	avl_for_each_element_safe(&services, s, avl, t) {
 		if (!s->host)
@@ -89,12 +101,9 @@ cache_gc_timer(struct uloop_timeout *timeout)
 			continue;
 		}
 		s->refresh += 50;
-		if (cache_service_is_host(s)) {
-			dns_send_question(s->iface, s->entry, TYPE_A, 0);
-			dns_send_question(s->iface, s->entry, TYPE_AAAA, 0);
-		} else {
-			dns_send_question(s->iface, s->entry, TYPE_PTR, 0);
-		}
+		if (cache_service_is_host(s))
+			continue;
+		dns_send_question(s->iface, s->entry, TYPE_PTR, 0);
 	}
 
 	uloop_timeout_set(timeout, 10000);

cache.h
@@ -44,6 +44,7 @@ struct cache_record {
 	uint16_t rdlength;
 	time_t time;
 	struct interface *iface;
+	int refresh;
 };
 
 extern struct avl_tree services;

commit f89986b67dd5256c6570e563c81aded4d39bc002

Fix refreshing cached A(AAA) records that expire
Old code was trying to refresh record after it has already expired. Now
it tries after 50% of TTL has passed - we do the same for other records.

Fixes: 80dd24602480 ("Refresh DNS records A and AAAA directly")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
Acked-by: John Crispin <john@phrozen.org>

cache.c
@@ -76,7 +76,7 @@ cache_gc_timer(struct uloop_timeout *timeout)
 	struct cache_service *s, *t;
 
 	avl_for_each_element_safe(&records, r, avl, p) {
-		if (!cache_is_expired(r->time, r->ttl, 100))
+		if (!cache_is_expired(r->time, r->ttl, r->refresh))
 			continue;
 		/* Records other and A(AAA) are handled as services */
 		if (r->type != TYPE_A && r->type != TYPE_AAAA) {
@@ -336,9 +336,11 @@ cache_answer(struct interface *iface, uint8_t *base, int blen, char *name, struc
 		if (!a->ttl) {
 			DBG(1, "D -> %s %s ttl:%d\n", dns_type_string(r->type), r->record, r->ttl);
 			r->time = now + 1 - r->ttl;
+			r->refresh = 100;
 		} else {
 			r->ttl = a->ttl;
 			r->time = now;
+			r->refresh = 50;
 			DBG(1, "A -> %s %s ttl:%d\n", dns_type_string(r->type), r->record, r->ttl);
 		}
 		return;
@@ -359,6 +361,7 @@ cache_answer(struct interface *iface, uint8_t *base, int blen, char *name, struc
 	r->rdlength = dlen;
 	r->time = now;
 	r->iface = iface;
+	r->refresh = 50;
 
 	if (tlen)
 		r->txt = memcpy(txt_ptr, rdata_buffer, tlen);