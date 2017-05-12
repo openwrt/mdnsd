openwrt/mdnsd

commit 70c66fbbcde868d2fe6034b5f706f1524e4f3d44

Fix sending replies to PTR questions
When we receive PTR question it includes hostname (instance), e.g.:
mdnsd: parse_question (391): Q -> PTR lede._http._tcp.local

First of all we should check if it matches hostname we use before trying
to reply. Secondly service_reply expects service with domain appended
(without hostname/instance) so we need to strip received string out of
hostname before passing it.

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
Acked-by: John Crispin <john@phrozen.org>

dns.c
@@ -367,9 +367,19 @@ parse_question(struct interface *iface, char *name, struct dns_question *q)
 		break;
 
 	case TYPE_PTR:
-		if (!strcmp(name, sdudp))
+		if (!strcmp(name, sdudp)) {
 			service_announce_services(iface, announce_ttl);
-		service_reply(iface, name, announce_ttl);
+		} else {
+			/* First dot separates instance name from the rest */
+			char *dot = strchr(name, '.');
+			/* Length of queried instance */
+			size_t len = dot ? dot - name : 0;
+
+			/* Make sure it's query for the instance name we use */
+			if (len && len == strlen(mdns_hostname) &&
+			    !strncmp(name, mdns_hostname, len))
+				service_reply(iface, dot + 1, announce_ttl);
+		}
 		break;
 
 	case TYPE_AAAA:

commit 49fdb9f7a5454f7e11e8bcd621bff17cbf9da5ca

Support PTR queries for a specific service
We should check if queried name starts with a _ to see if it is about a
specific service + domain.

Fixes: 70c66fbbcde86 ("Fix sending replies to PTR questions")
Reported-by: Cristian Morales Vega <cristian@samknows.com>
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

dns.c
@@ -378,15 +378,18 @@ parse_question(struct interface *iface, struct sockaddr *from, char *name, struc
 			dns_reply_a(iface, to, announce_ttl);
 			service_announce_services(iface, to, announce_ttl);
 		} else {
-			/* First dot separates instance name from the rest */
-			char *dot = strchr(name, '.');
-			/* Length of queried instance */
-			size_t len = dot ? dot - name : 0;
+			if (name[0] == '_') {
+				service_reply(iface, to, NULL, name, announce_ttl);
+			} else {
+				/* First dot separates instance name from the rest */
+				char *dot = strchr(name, '.');
 
-			/* Make sure it's query for the instance name we use */
-			if (len && len == strlen(umdns_host_label) &&
-			    !strncmp(name, umdns_host_label, len))
-				service_reply(iface, to, NULL, dot + 1, announce_ttl);
+				if (dot) {
+					*dot = '\0';
+					service_reply(iface, to, name, dot + 1, announce_ttl);
+					*dot = '.';
+				}
+			}
 		}
 		break;
 